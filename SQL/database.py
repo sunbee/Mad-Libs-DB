5ed154ba259230ba4ff90c8634f18315d8f1e54c
sunbee <sanjay.bhatikar@gmail.com>
2023-01-08 23:01:48 -0600

    Updated database.py

--- SQL/database.py
+++ SQL/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://postgres:egregious@127.0.0.1:5432/madlib_bonanza'
+SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://postgres:egregious@127.0.0.1:5432/madlib_cornucopia'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
